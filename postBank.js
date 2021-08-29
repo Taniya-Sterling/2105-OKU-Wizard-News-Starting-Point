665d6e5c51ce522e7d4a4e273c57c6d48a2c5e64
Taniya Sterling <sterlingtaniya@gmail.com>
2021-08-28 21:23:25 -0400

    added new messages

---
+++ postBank.js
@@ -0,0 +1,57 @@
+
+const data = [
+  { id: 1, upvotes: 257, title: "METAHUMAN SUMMIT!", content: 
+"Metahumans-Humans with extraordinary powers have been increasing over the past few decades at an exponential rate…with this growth also comes difficulties With ongoing fears and tensions rising the UNITED NATIONS, finally scheduled a Metahuman summit to talk about the protections and precautions for Metahumans. Prominent members of the Justice League and open metahumans such as STATIC SHOCK and BLACK CANARY are reported to be in attendance as well as the nation of Bialya who were once accused of kidnapping Metahumans to use as a military force…when asked to comment the Bialyan Public relations officer stated: 'Bialya denies all responsibility of such an occurrence, these actions were done by a rebel group in our country, we Bialyans of course welcome Metahumans to our country with open arms ready to give them all the freedom they deserve'. That statement which we were assured is honest is echoed by many other ones such as Lex Luthor, Count Vertigo and Simon Stagg who all promise to use their extensive wealth for the betterment of Metahuman kind. This article is developing and will be updated as new information is gained.", 
+
+name: "Zach Carrie~ Daily Planet", 
+date: new Date(Date.now() - 15000000) },
+
+  { id: 2, upvotes: 221, title: "NEW SPEEDSTER?", 
+content: 
+"A white blur just ran past me I thought I was hallucinating it! My Radar barely responded but the blur was moving three-four hundred miles per hour! 'Officer William Davy, MCPD. That quote from Officer Davy coincides with additional reports of sightings around the Metropolis this past week, reports of a white blur blazing past parks leaving residents with one questionIs there a new speedster in town? And what are they doing away from Central city? Home of the Flash and related speeding heroes? This is a developing article as eyewitnesses have reported a dog with a cape near these events as well as Superboy inquiring as to what is in the Dog’s mouth before said dog disappears in a white blur. More will be added as the situation develops.",
+
+name: "Miles A. Head~Daily Planet", 
+date: new Date(Date.now() - 900000) },
+
+  { id: 3, upvotes: 171, title: "Superhero look-alike challenge!?", 
+content:
+"The trend sweeping across the nation! After Daily Planet’s own` Clark Kent was caught without his glasses bearing a startling likeness to Metropolis\’ favorite son-Superman, the challenge *Superhero Look-Alike* has been all over all forms of social media. Even Gotham’s own resident Billionaire Playboy-Bruce Wayne took to the challenge! Insisting hilariously his likeness to the caped superhero-Superman. Check out the top twenty Celebrity \‘Look-Alikes\’ on our Twitter  @DAILYPLANET for more laughs!", 
+name: "Lois Lane Daily ~Planet senior journalist",  
+date: new Date(Date.now() - 5000) },
+
+  { id: 4, upvotes: 166, title: "GOTHAM CONFIRMED AS SECOND MOST DANGEROUS CITY IN U.S",
+content: 
+"It seems the incident surrounding the Toyman in Gotham came at an all too convenient time as for the 30th year in the road Gotham City as ranked in the top ten of the most dangerous cities in America. After claiming the 2nd spot for the 16th consecutive year in the row, Daily Planet reporters asked residents what they think about this ranking. \'I mean it’s not much but y’know, ima actually kind of proud to know that Gotham is still in the top three\' - Lenard *Lefty* Mahome \'It just goes to show the grit us Gothamites have, I mean people from central city or metropolis have those spandex wearing ‘villains’ but we have literal psychopaths as our heroes so we just have a different sort of integrity.\' - Darlene *Dusty* Rose. Truly the people of Gotham have an unbending will and strength…to see more about the Toyman incident check out our article ‘TOYMAN TUMBLE’",
+name: "Jimmy Olsen ~Daily Planet ",
+date: new Date(Date.now() - 10000000) },
+
+  { id: 5, upvotes: 145, title: "TOYMAN’S TUMBLE", 
+content: "'You’re a little far from home aren’t ya?' Those words were spoken by Gabrielle Dubois, A Gotham city native and a witness to the Toyman’s attempted robbery of a Bank. One week ago Metropolis’ own Toyman attempted to rob the June and Patterson bank on 76th street in Gotham City…his attempt was swiftly halted by many brave civilians. Eyewitnesses report nearly every single able-bodied man and woman pelting the Toymaker -who was atop the car he used to ram the building- until he fell to ground, after the tumble bystanders the proceeded to knock the Toymaker unconscious. No one was harmed aside the Toymaker who was taken away with severe injuries, when interviewing college student Majorie Major she stated.'It’s Gotham, being outside after sundown is dangerous so you gotta know how to protect yourself, taking down a Metropolis villain is easy when compared to running from killer croc' Once again Gotham has proven themselves a dangerous city with dangerous villains and even more dangerous civilians living in it.", 
+name: "Kai Sterling ~Daily Planet",
+date: new Date(Date.now() - 5000000) },
+
+  { id: 6, upvotes: 126, title: "A CITY IN MOURNING.", 
+  content: 
+"A city with underpaid office workers crying in the streets.Fires being set as masked teens run away from lit garbage cans.Hundreds outside the mayors office screaming in protest ordering their leaders to do something about their affliction. This describes the scenes in Gotham this past week as the city mourns the lost of its beloved ‘Batburger’ In a attack that took everyone by surprise the small-time villain known as the ‘Condiment King’ stole all of the Ketchup, Bat-Becue sauce  and Tartar sauce needed in the ‘Riddle me Fish’ from the trucks enroute to locations across the city. Without the needed sauce to accompany their meals Batburgers across the city have limited sauce to ‘1’ per person or outright closed Citizens are outraged by the attack against their popular lunch spot and under sauced Night-wings. Anarchy reigns as it is reported that many citizens are seeking revenge against the Condiment king who has not yet been located…Our thoughts and prayers go out to the hangry citizens of Gotham city.", 
+name: "Carlos Everett  ~Daily Planet", 
+date: new Date(Date.now() - 600000) },
+
+  { id: 7, upvotes: 114, title: "LEX LUTHOR! NEW MAN? ", content: "Metropolis\' own billionaire has turned over a new leaf. After dozens attacks again Superman, numerous attempts to take over the city, accounts of corporate espionage, paid assassinations, billions of dollars in property damage, blackmail, Arson, Kidnapping, Robbery, Perjury, Corporal injury, Conspiracy, Extortion and downright treason- Lex Luthor has defied all odds and has been released from Belle Reve penitentiary! Upon his return the businessman promises to use his money and resources to help others! He’s also announced new job opportunities as he has recently opened a new mine where odd glowing green stones have been reported.Mister Luthor assured the public that this gem is not Kyrptonite of course since being in possession of the very mineral which weakens Superman’s powers would indeed violate his parole. The city looks in excitement as Lex Luthor-New man marches forward to \'Take over the city with charm.\' As quoted by mister Luthor. We all look forward to see how he will help the city and contribute to the peace and prosperity of our own Metropolis!" 
+,
+name: "Poppy Cox~ Daily Planet", 
+date: new Date(Date.now() - 60000000) },
+
+  { id: 8, upvotes: 102, title: "BANK ROBBERY STOPPED BY SUPERGIRL!", content: "Bank robbers were brought to a screeching halt when everyone’s favorite Super-Supergirl stepped in to intervene with a new furry addition resources have reported as ‘Krypto the super dog’ a dog which has no relations to the numerous white blur related accidents which has caused thousands of dollars in property damage due to squirrels Supergirl shows her years of experience in the field with her quick suppression of the enemies while showing the rookie the ropes. After the robbers were arrested we got to ask Supergirl the question that has been weighing on everyone’s mind! Is she planning on joining a superhero team like her counterpart Superman? She was quite private about the details only stating that she “has gotten a few offers.” The possibility of the young hero joining the Justice league is pretty high! No matter where she goes though, Metropolis’ favorite Super will always have a home with us."
+, 
+name: "Clark Kent ~Daily Planet senior journalist", 
+date: new Date(Date.now() - 3000000) },];
+   
+const list = () => {
+  return [...data] // Notice that we're returning a copy of the array, so the original data is safe. This is called 'immutability'.
+};
+  const find = (id) => {
+    const post = data.find(post => post.id === +id);
+    return {...post}; // Again, we copy the post data before returning so the original information is safe.
+  }
+  
+  module.exports = { list: list, find: find };
